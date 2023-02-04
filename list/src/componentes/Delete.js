bf225140de8428bb026661d86135fb2b6d5cd5f7
newberg85 <bergviana12345@gmail.com>
2023-02-03 22:15:26 -0300

    new commit

--- list/src/componentes/Delete.js
+++ list/src/componentes/Delete.js
@@ -1,4 +1,4 @@
-import React from 'react'
+/*import React from 'react'
 import { DataContext } from './DataProvider';
 
 
@@ -12,4 +12,4 @@ export default function Delete() {
   return (
     <button id='delete' onClick={deletar}></button>
   )
-}
+}*/
